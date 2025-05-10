07b8652a2fa9742a6e72e5b6edb88bed03a8bbfb
Aras Ateşer <43793683+arasateser@users.noreply.github.com>
2025-05-10 15:38:44 +0300

    base business interfaces

---
+++ Business/Abstract/IAlbumService.cs
@@ -0,0 +1,17 @@
+﻿using Core.Utilities.Results.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IAlbumService
+    {
+        IDataResult<List<Album>> GetAllAlbums();
+        IDataResult<Album> GetByAlmbumId();
+        IResult AddAlbum(Album album);
+    }
+}
---
+++ Business/Abstract/IArtistService.cs
@@ -0,0 +1,17 @@
+﻿using Core.Utilities.Results.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IArtistService
+    {
+        IDataResult<List<Artist>> GetAllArtists();
+        IDataResult<Artist> GetByArtistId(int artistId);
+        IResult AddArtist(Artist artist);
+    }
+}
