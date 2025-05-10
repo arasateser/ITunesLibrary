4937b5776229574e58f4ed7f24106ce288b698e0
Aras Ateşer <43793683+arasateser@users.noreply.github.com>
2025-05-10 21:04:53 +0300

    Data Access Layer logic

---
+++ Business/Concrete/TrackManager.cs
@@ -0,0 +1,31 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class TrackManager : ITrackService
+    {
+
+        public IDataResult<List<Track>> GetAllTracks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<Track> GetByTrackId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataResult<List<Track>> GetTracksByMiliseconds(int min, int max)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataAccess/Concrete/ChinookContext.cs
@@ -0,0 +1,23 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete
+{
+    public class ChinookContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;
+                Database=Chinook;
+                Trusted_Connection=true");
+        }
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Artist> Artist { get; set; }
+        public DbSet<Track> Tracks { get; set; }
+    }
+}
